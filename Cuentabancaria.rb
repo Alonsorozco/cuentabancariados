639fec9d7a9cd240444800b1141d77301ed376ef
alonzorozco <sfabian.orozco@gmail.com>
2021-07-04 18:18:33 -0400

    add:1.1

--- Cuentabancaria.rb
+++ Cuentabancaria.rb
@@ -1,44 +1,44 @@
-class Usuario
-    attr_accessor :cuentas
-    def initialize (nombre, cuentas)
-        @nombre = nombre
-        @cuentas = cuentas
+class User
+    attr_accessor :count
+    def initialize (name, count)
+        @name = name
+        @count = count
 
     end
     #suma de saldo total
-    def saldo_total
-        saldo= 0
-        self.cuentas.each do |i|
-            saldo+= i.saldo
+    def total_balance
+        balance= 0
+        self.count.each do |i|
+            balance+= i.balance
         end
-        return saldo
+        return balance
     end
 end
 
 
-class CuentaBancaria
-    attr_accessor :saldo, :numero_de_cuenta
-    def initialize(banco,numero_de_cuenta,saldo=0)
-        @banco= banco
-        @numero_de_cuenta = numero_de_cuenta
-        @saldo = saldo
+class Bankaccount
+    attr_accessor :balance, :number_count
+    def initialize(bank,number_count,balance=0)
+        @bank= bank
+        @number_count = number_count
+        @balance = balance
     end
-    def transferir (monto, otra_cuenta)
-        self.saldo -= monto
-        otra_cuenta.saldo += monto
+    def transfer (amount, another_account)
+        self.balance -= amount
+        another_account.balance += amount
     end
 end
 
 
-a = CuentaBancaria.new('Itau', 15484565, 5000)
-b = CuentaBancaria.new('Estado', 12545652, 5000)
+a = Bankaccount.new('Itau', 15484565, 5000)
+b = Bankaccount.new('Estado', 12545652, 5000)
 
 #transferir a otra cuenta 
-pp a.transferir(500,b)
+pp a.transfer(500,b)
 
 #creando un nuevo usario
-u1 = Usuario.new('Fabian', [a])
+u1 = User.new('Fabian', [a])
 pp u1
 #saldo total
-total = u1.saldo_total
+total = u1.total_balance
 pp "Fabian su saldo total es: #{total}"
